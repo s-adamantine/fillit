4cb8271ea7d913a5bf7da40848cbe05a3d6471a8
sadamant <sharfy.adamantine@gmail.com>
2017-10-26 23:38:36 -0700

    got rid of the hardcoded tets in favor of connection count

--- fillit/insert.c
+++ fillit/insert.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   insert_array.c                                     :+:      :+:    :+:   */
+/*   insert.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgaia <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: sadamant <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/11 13:18:31 by rgaia             #+#    #+#             */
-/*   Updated: 2017/10/25 18:15:19 by sadamant         ###   ########.fr       */
+/*   Created: 2017/10/26 23:37:16 by sadamant          #+#    #+#             */
+/*   Updated: 2017/10/26 23:37:25 by sadamant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,31 @@ static int	count_hash(char *t)
 	if (count != 4)
 		return (0);
 	return (1);
+}
+
+static int	connections(char *t)
+{
+	int	i;
+	int	connect;
+
+	i = 0;
+	connect = 0;
+	while (t[i])
+	{
+		if (t[i] == '#')
+		{
+			if ((i + 1 < 20) && t[i + 1] == '#')
+				connect++;
+			if ((i - 1 >= 0) && t[i - 1] == '#')
+				connect++;
+			if ((i + 5 < 20) && t[i + 5] == '#')
+				connect++;
+			if ((i - 5 >= 0) && t[i - 5] == '#')
+				connect++;
+		}
+		i++;
+	}
+	return (connect == 6 || connect == 8);
 }
 
 //input: unreduced tetrimino string
@@ -55,19 +80,26 @@ static int	tet_width(char *t)
 	return (r - l + 1);
 }
 
-static int	isvalid_tetrimino(char	*tetrimino)
+char	*reduce_tetrimino(char *t)
 {
-	char	**hardcoded;
+    int     count;
+    char    *reduced;
+    char    *reducedcpy;
 
-	if(!count_hash(tetrimino))
-		return (0);
-	hardcoded = valid_tetriminos();
-	while (*hardcoded)
-	{
-		if (ft_strcmp(reduce_tetrimino(tetrimino), *hardcoded++) == 0)
-			return (1);
-	}
-	return (0);
+    count = 0;
+    t = strchr(t, '#');
+    reduced = (char *)malloc(sizeof(char) * 14);
+    reducedcpy = reduced;
+    while (*t && count < 4)
+    {
+        if (*t == '\n')
+            t++;
+        if (*t == '#')
+            count++;
+        *reduced++ = *t++;
+    }
+    *reduced = '\0';
+    return (reducedcpy);
 }
 
 static t_tet	*get_next_tetrimino(char c, char *str)
@@ -87,12 +119,12 @@ static t_tet	*get_next_tetrimino(char c, char *str)
 	return (t);
 }
 
-// an array of valid tetriminos
+// an array of valid tetrimino structs
 t_tet			**insert_array(char *buf)
 {
 	int		i;
 	char	letter;
-	char	*str;
+	char	*tet;
 	t_tet	**tetriminos;
 
 	i = 0;
@@ -101,10 +133,10 @@ t_tet			**insert_array(char *buf)
 		return (NULL);
 	while (*buf)
 	{
-		str = ft_strnew(20);
-		str = ft_strncpy(str, buf, 20);
-		if (isvalid_tetrimino(str))
-			tetriminos[i++] = get_next_tetrimino(letter++, str);
+		tet = ft_strnew(20);
+		tet = ft_strncpy(tet, buf, 20);
+		if (count_hash(tet) && connections(tet))
+			tetriminos[i++] = get_next_tetrimino(letter++, tet);
 		else
 			return (NULL);
 		buf = buf + 21;
